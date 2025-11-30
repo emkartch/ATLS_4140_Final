d6a65f45a3f13ac208c32021049f04091234228a
Josie-Armstrong <156936598+Josie-Armstrong@users.noreply.github.com>
2025-11-30 16:29:50 -0700

    Added orange tiles, started on tile switches

--- the-road-to-rainbow/tilemaps.gd
+++ the-road-to-rainbow/tilemaps.gd
@@ -2,37 +2,60 @@ extends TileMapLayer
 signal activate_puzzle(tile_coords, tileset_coords)
 
 # for tracking where we are at
-var lvl = 1
+var lvl = 2
 var color = "red"
 var lvl_finished = false
 var num_finished = 0
 
+var lvl_tiles = []
 var puzzle_coords = [Vector2i(2, 4), Vector2i(3, 4), Vector2i(4, 4)]
 
 func _ready():
+	lvl_tiles = [$Red, $Orange]
+	
+	lvl = 1
 	change_tile_set()
+
+# Setting show and collisions per tile
+func set_collisions():
+	for i in range(0,2):
+		print(lvl_tiles[i])
+		print(i)
+		
+		if lvl - 1 == i:
+			lvl_tiles[i].show()
+			lvl_tiles[i].collision_enabled = true
+		else:
+			lvl_tiles[i].hide()
+			lvl_tiles[i].collision_enabled = false
 
 func change_tile_set():
 	if lvl == 1 and lvl_finished == false:
 		var newTexture = load("res://tilesets/RedTilesGrey.png")
-		$redGround.tile_set.get_source(0).texture = newTexture
-		$redAccPuzz.tile_set.get_source(1).texture = newTexture
+		$Red/Ground.tile_set.get_source(0).texture = newTexture
+		$Red/AccPuzz.tile_set.get_source(1).texture = newTexture
 	elif lvl == 1 and lvl_finished == true:
 		var newTexture = load("res://tilesets/OnlyRed-Lvl1.png")
-		$redGround.tile_set.get_source(0).texture = newTexture
-		$redAccPuzz.tile_set.get_source(1).texture = newTexture
-
+		$Red/Ground.tile_set.get_source(0).texture = newTexture
+		$Red/AccPuzz.tile_set.get_source(1).texture = newTexture
+	elif lvl == 2 and lvl_finished == false:
+		var newTexture = load("res://tilesets/OrangeTiles.png")
+		$Orange/Ground.tile_set.get_source(0).texture = newTexture
+		
+	# setting show and collisions
+	set_collisions()
+	
 # Checks if mouse clicked on a puzzle tile
 func check_for_puzzle_click(sprite_pos):
 	
 	# Where is the mouse in global coords?
 	var mouse_pos = get_global_mouse_position()
 	# Where is the tile on the tilemap layer?
-	var tile_pos = $redAccPuzz.local_to_map(mouse_pos)
+	var tile_pos = $Red/AccPuzz.local_to_map(mouse_pos)
 	# Where is the tile in global coords?
-	var tile_map_coords = $redAccPuzz.map_to_local(tile_pos)
+	var tile_map_coords = $Red/AccPuzz.map_to_local(tile_pos)
 	# Where is the tile in the tileset?
-	var tileset_coords = $redAccPuzz.get_cell_atlas_coords(tile_pos)
+	var tileset_coords = $Red/AccPuzz.get_cell_atlas_coords(tile_pos)
 	print(tileset_coords)
 	
 	if tileset_coords!= Vector2i(-1,-1):
