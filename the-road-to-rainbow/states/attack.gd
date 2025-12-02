2939a482127ccfc1089999676e2153f2b4e8bfc6
emkartch <elka9063@colorado.edu>
2025-12-02 15:59:22 -0700

    Update attack.gd

--- the-road-to-rainbow/states/attack.gd
+++ the-road-to-rainbow/states/attack.gd
@@ -11,16 +11,3 @@ func enter():
 
 func _on_attack_finished():
 	Transitioned.emit(self, "idle")
-
-#func update(_delta):
-	#if animatedSprite.get_animation() == "attack_" + Global.cur_direction:
-		#pass
-	#else:
-		#animatedSprite.play("attack_" + Global.cur_direction)
-#
-#func physics_update(_delta):
-	#
-	#if !Global.player_move:
-		#Transitioned.emit(self, "idle")
-	#elif Global.player_move and Input.is_action_just_released("player_run"):
-		#Transitioned.emit(self, "walk")
