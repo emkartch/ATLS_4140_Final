39dec6958a0e38f1441630dfe56325ab0dc8b1e8
emkartch <elka9063@colorado.edu>
2025-12-03 13:00:05 -0700

    Update attack.gd

--- the-road-to-rainbow/states/attack.gd
+++ the-road-to-rainbow/states/attack.gd
@@ -5,9 +5,11 @@ class_name attack
 
 @onready var animatedSprite: AnimatedSprite2D = get_node("/root/Main/Player/AnimatedSprite2D")
 
+func _ready():
+	animatedSprite.animation_finished.connect(_on_attack_finished)
+	
 func enter():
 	animatedSprite.play("attack_" + Global.cur_direction)
-	animatedSprite.animation_finished.connect(_on_attack_finished)
 
 func _on_attack_finished():
 	Transitioned.emit(self, "idle")
