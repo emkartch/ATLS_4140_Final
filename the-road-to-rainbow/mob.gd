7a7b51f65c85d74bea1eea5908ac938a4338290a
emkartch <elka9063@colorado.edu>
2025-12-05 11:36:19 -0700

    All Mob Animations Updated and Added

--- the-road-to-rainbow/mob.gd
+++ the-road-to-rainbow/mob.gd
@@ -6,7 +6,6 @@ var health = 3
 @onready var player = get_node("/root/Main/Player")
 #@onready var camera = get_node("/root/Main/Player/Camera2D/CameraArea")
 #@onready var camera = get_node("/root/Main/Player/ReferenceRect")
-@onready var animatedSprite: AnimatedSprite2D = get_node("/root/Main/Mob/AnimatedSprite2D")
 var r_slimes: SpriteFrames = preload("res://mobs/sprites/red_slime.tres")
 var o_slimes: SpriteFrames = preload("res://mobs/sprites/orange_slime.tres")
 var y_slimes: SpriteFrames = preload("res://mobs/sprites/yellow_slime.tres")
@@ -25,32 +24,36 @@ func _ready():
 	#move.connect(player_velocity)
 	$AnimatedSprite2D.sprite_frames = slime_colors[Global.game_lvl - 1]
 	$AnimatedSprite2D.play("idle")
+	$AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(_delta: float) -> void:
 
 	if track_color != Global.game_lvl:
 		change_color()
 		track_color += 1
-
-	if knockbackBool == true:
-		var kb_direction = (Global.knockback - velocity).normalized() * 10000
-		velocity = kb_direction
-		velocity.y -= 250
-		move_and_slide()
-		knockbackBool = false
-	else:
-		var direction = global_position.direction_to(player.global_position)
-		velocity = direction * speed
-		move_and_slide()
-		animatedSprite.play("walk")
+		
+	if health != 0:
+		if knockbackBool == true:
+			var kb_direction = (Global.knockback - velocity).normalized() * 10000
+			velocity = kb_direction
+			velocity.y -= 250
+			move_and_slide()
+			knockbackBool = false
+		else:
+			var direction = global_position.direction_to(player.global_position)
+			velocity = direction * speed
+			move_and_slide()
+			$AnimatedSprite2D.play("walk")
 		
 	
 func mob_take_damage():
 	health -= 1
 	knockbackBool = true
 	if health == 0:
-		
-		queue_free()
+		$AnimatedSprite2D.play("death")
 
 func change_color():
-	$AnimatedSprite2D.sprite_frames = slime_colors[Global.game_lvl]
+	$AnimatedSprite2D.sprite_frames = slime_colors[Global.game_lvl - 1]
+
+func _on_animation_finished():
+	queue_free()
